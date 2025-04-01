8017751f4648ac08184f857a554491594c4a52b1
H-web-lvr <halima.moussa@edu.esiee-it.fr>
2025-04-01 14:10:51 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,8 +8,8 @@ import sqlite3
 app = Flask(__name__)                                                                                                                 
                                                                                                                                        
 @app.route('/')
-def hello_world():
-    return render_template('hello.html')
+def MaPremiereAP():
+    return "<h2>Ma page de contact</h2>"
   
 if __name__ == "__main__":
   app.run(debug=True)
